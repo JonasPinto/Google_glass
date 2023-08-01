273c06726968b02b2831a39825a80cb7a227864d
Jonas Silva <jonas.silva@easolucinfo.com>
2023-08-01 01:10:17 -0300

    atualizações recentes

--- javascript/funcoes.js
+++ javascript/funcoes.js
@@ -1,3 +1,8 @@
 function mudaFoto (foto){
     document.getElementById("icone").src = foto
 }
+
+function atualiza () {
+    atualizacao = 'Atualizado em 30/05/2022'
+    console.log(atualizacao)
+}
